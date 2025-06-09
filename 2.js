5c4114680ec222d2cdf966e1c75d84e2b7f91c7f
Gia Bao IT <tranvangiabao96206@gmail.com>
2025-06-09 22:24:45 +0700

    Fix k dùng file .env

--- 2.js
+++ 2.js
@@ -54,7 +54,7 @@ app.set("views", path.join(__dirname, "views"));
 
 router(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
